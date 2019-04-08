eb6694b65b2693dcdb4c5eee89d993f0a54172c5
skoddy <m.t.skodowski@gmail.com>
2019-04-08 15:16:35 +0200

    ...

--- app/src/main/java/com/skottie/myfirebaseapp/MainActivity.java
+++ app/src/main/java/com/skottie/myfirebaseapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.skottie.myfirebaseapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,8 +14,11 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mStatusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        checkCurrentUser();
+        final Button signInButton = findViewById(R.id.button_signin);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignIn();
+            }
+        });
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
@@ -51,5 +65,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void checkCurrentUser() {
+        // [START check_current_user]
+        mStatusTextView = findViewById(R.id.statusText);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+        } else {
+            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt));
+        }
+        // [END check_current_user]
+    }
+    public void openSignIn() {
+        Intent intent = new Intent(this, EmailPasswordActivity.class);
+        startActivity(intent);
     }
 }
